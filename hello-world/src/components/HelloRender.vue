83970bf3e7b598b350fc61cf1f625b8443bb1bc8
engvuchen <1742284391@qq.com>
2023-11-14 10:53:43 +0800

    U 修改demo

--- hello-world/src/components/HelloRender.vue
+++ hello-world/src/components/HelloRender.vue
@@ -1,28 +1,22 @@
 <script type="text/javascript">
 export default {
-  name: 'HelloRender',
-  // functional: true,
+  name: 'HelloWorld',
   props: {
     msg: String,
   },
-  render(createElement) {
-    return createElement(
+  render: function(createElement) {
+    return createElement('div', [
+      createElement(
       'h1',
       {},
       this.msg,
-      createElement('p', {}, [
-        'For a guide and recipes on how to configure / customize this project,<br/>check out the',
-        createElement(
-          'a',
-          {
-            href: 'https://cli.vuejs.org',
-            target: '_blank',
-            rel: 'noopener',
-          },
-          'vue-cli documentatio'
-        ),
-      ])
-    );
+    ),
+    createElement(
+      'h2',
+      {},
+      this.msg,
+    )
+    ])
   },
 };
 </script>
