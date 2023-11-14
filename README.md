2b2ca942440f1424625fd7fdbf3d4b118131e7a3
engvuchen <31369318+engvuchen@users.noreply.github.com>
2023-11-14 10:28:31 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue2-render-demo
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/engvuchen/vue2-render-demo)
---
+++ hello-world/src/components/HelloRender.vue
@@ -0,0 +1,46 @@
+<script type="text/javascript">
+export default {
+  name: 'HelloRender',
+  // functional: true,
+  props: {
+    msg: String,
+  },
+  render(createElement) {
+    return createElement(
+      'h1',
+      {},
+      this.msg,
+      createElement('p', {}, [
+        'For a guide and recipes on how to configure / customize this project,<br/>check out the',
+        createElement(
+          'a',
+          {
+            href: 'https://cli.vuejs.org',
+            target: '_blank',
+            rel: 'noopener',
+          },
+          'vue-cli documentatio'
+        ),
+      ])
+    );
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
